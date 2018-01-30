8f100ce5d9d69152dc855de897866d958a7385f0
fxwalsh <fxwalsh@wit.ie>
2018-01-30 10:25:21 +0000

    last commit

--- ewd_labs/node_lab4/start_client.js
+++
@@ -1,5 +0,0 @@
-const args = [ 'start' ];
-
-const opts = { stdio: 'inherit', cwd: 'C:\repos\enterprise-web-dev\ewd_labs_2017\node_lab2\hackerNews', shell: true };
-
-require('child_process').exec('npm start');
